31314a84db4d25d9c39fa832c67a2b2675feb87d
Leonardo Coelho <55851495+LeonardCoelho@users.noreply.github.com>
2025-08-14 20:58:31 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import joblib
 
 # Carregar modelo treinado
-modelo = joblib.load('modelo_random_forest_tunado.pkl')
+path = os.path.join(os.path.dirname(__file__), "src", "modelo_random_forest_tunado.pkl")
+modelo = joblib.load(path)
 
 # Título e descrição
 st.set_page_config(page_title="Previsão de Frete", layout="centered")
